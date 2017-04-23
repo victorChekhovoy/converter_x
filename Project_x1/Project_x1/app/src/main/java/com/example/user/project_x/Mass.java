491f0ae2213564e134e67f0d62fab463ce69219d
Grey-Fullbuster <dmytro.ivakhnenkov@gmail.com>
2017-04-23 20:37:55 +0300

    Working program

---
+++ Project_x1/Project_x1/app/src/main/java/com/example/user/project_x/Mass.java
@@ -0,0 +1,164 @@
+package com.example.user.project_x;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Mass extends AppCompatActivity {
+    String[] data = {"tons", "kilograms", "grams", "milligrams", "pounds", "ounces", "carats", "stones"};
+    Map<String, Double> types = new TreeMap<>();
+    static String from = "";
+    static String to = "";
+    static String input= "";
+    static EditText in;
+    static TextView answer;
+    static int i=0;
+    static int j=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mass);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
+        type_init();
+        answer = (TextView)findViewById(R.id.textView2_m);
+        answer.setText("result");
+        final EditText in = (EditText)findViewById(R.id.editText_m);
+        spinner_from_init(adapter);
+        spinner_to_init(adapter);
+        in.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                input = (in.getText()).toString();
+                Check a = new Check();
+                if(a.check(input) && input.length()>=1){
+                    if(input.equals(".") && input.length()<2){
+                        Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();
+                    }
+                    else{Mass_convetrer(from, to, input);}}
+                else{
+                    if(a.check(input)==false && input.length()>=1){Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();}
+                    else{answer.setText("result");}}}
+        });}
+    @Override
+    protected void onStart() {
+        i=0;
+        j=0;
+        super.onStart();
+    }
+    void type_init(){
+        types.put("tons", 1000.0);
+        types.put("kilograms", 1.0);
+        types.put("grams", 0.001);
+        types.put("milligrams", 0.000001);
+        types.put("pounds", 0.45359237);
+        types.put("ounces", 0.0283495231);
+        types.put("carats", 0.0002);
+        types.put("stones", 6.35029318);
+    }
+    void spinner_from_init(ArrayAdapter a){
+        Spinner from_spinner = (Spinner) findViewById(R.id.from_type_m);
+        from_spinner.setAdapter(a);
+        from_spinner.setPrompt("Select type");
+        from_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                from = data[position];
+                Check a = new Check();
+                if(i<1){i+=1;}
+                else{
+                if(a.check(input) && input.length()>=1){
+                    if(input.equals(".") && input.length()<2){
+                        Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();
+                    }
+                    else{Mass_convetrer(from, to, input);}}
+                else{
+                    if(a.check(input)==false && input.length()>=1){Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();}
+                    else{answer.setText("result");}}
+            }}
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });}
+    void spinner_to_init(ArrayAdapter a){
+        Spinner to_spinner = (Spinner)findViewById(R.id.spinner_m);
+        to_spinner.setAdapter(a);
+        to_spinner.setPrompt("Select type");
+        to_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                to = data[position];
+                Check a = new Check();
+                if(j<1){j+=1;}
+                else{
+                if(a.check(input) && input.length()>=1){
+                    if(input.equals(".") && input.length()<2){
+                        Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();
+                    }
+                    else{Mass_convetrer(from, to, input);}}
+                else{
+                    if(a.check(input)==false && input.length()>=1){Toast.makeText(getApplicationContext(), "bad symbols", Toast.LENGTH_SHORT).show();}
+                    else{answer.setText("result");}}
+            }}
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    void Mass_convetrer(String from, String to, String amount){
+        double from_amount = Double.parseDouble(amount);
+        double to_amount = types.get(from)*from_amount/types.get(to);
+        if(to_amount%1 == 0){long myInt = (long) to_amount;
+            String output = Long.toString(myInt);
+            answer.setText(output);}
+        else{
+            String output = Double.toString(to_amount);
+            answer.setText(output);}
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_1:
+                finish();
+                return true;
+            case R.id.action_2:
+                Intent intent1 = new Intent(this, Authors.class);
+                startActivity(intent1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }}
+
+
